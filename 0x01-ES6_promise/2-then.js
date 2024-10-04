6eed670f249a0b5509ee56f3647280ae4ae209ce
mrxdoffical <berogergesg@gmail.com>
2024-10-04 22:17:05 +0300

    task2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  }).catch(() => {
-    console.error();
-    return new Error();
-  }).finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
