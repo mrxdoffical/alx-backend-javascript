df23dad2e72ea090ea08efd4b139fd75392b4107
mrxdoffical <berogergesg@gmail.com>
2024-10-05 16:07:14 +0300

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbortonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // setters
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
+      throw new Error('Students must be an array of Strings');
+    }
+    this._students = newStudents;
+  }
+}
