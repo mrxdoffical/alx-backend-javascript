79457131557ddff059bd42a2320c5e69a8a58bd1
mrxdoffical <berogergesg@gmail.com>
2024-10-07 13:07:21 +0300

    task0 --test

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'san francisco' },
+    { id: 2, firstName: 'James', location: 'columbia' },
+    { id: 5, firstName: 'Serena', location: 'san francisco' },
+  ];
+}
