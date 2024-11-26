d0d2a3d305518d72a34bb77a06bf36e6df2ab0fe
mrxdoffical <berogergesg@gmail.com>
2024-11-26 16:42:28 +0200

    task2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const file = fs.readFileSync(path);
+    const fileContent = file.toString();
+	const lines = fileContent.split('\n');
+
+	const students = {
+		cs: [],
+		swe: []
+	};
+
+	let totalStud = 0;
+
+	lines.forEach((line, index) => {
+		if (line && index > 0) {
+			const studata = line.split(',');
+			const fn = studata[0];
+			const field = studata[3];
+
+			if (field === 'CS'){
+				students.cs.push(fn);
+				totalStud++;
+			} else if(field === 'SWE'){
+				students.swe.push(fn);
+				totalStud++;
+			}
+
+		}
+	});
+
+	console.log(`Number of students: ${totalStud}`);
+	console.log(`Number of students in CS: ${students.cs.length}. List: ${students.cs.join(', ')}`);
+	console.log(`Number of students in SWE: ${students.swe.length}. List: ${students.swe.join(', ')}`);
+
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
