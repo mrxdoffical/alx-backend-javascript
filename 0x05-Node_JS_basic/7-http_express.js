d235756cdef80b628282d5094b8425c733ed7cfb
mrxdoffical <berogergesg@gmail.com>
2024-11-27 00:29:23 +0200

    task7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const output = await countStudents(database);
+    res.send(`This is the list of our students\n${output}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
